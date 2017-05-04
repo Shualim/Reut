931dfcf97d75cb229e1a7929cb0c336f4757eaf6
yotam ronen <yotronen@gmail.com>
2017-05-04 14:49:26 +0300

    dayTime page and profilepage

--- Reut/src/pages/profile/profile.ts
+++ Reut/src/pages/profile/profile.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController) {
-
+  id: any;
+  constructor(public navCtrl: NavController) {}
+  createUser() {
+    console.log(this.id);
   }
 
 }
